823e0e15693da58fcc07e7d15d903057b8247d8a
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-08-28 09:36:52 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define ('host',  'chromelocalhost.mysql.database.azure.com');
+define ('dbhost',  'chromelocalhost.mysql.database.azure.com');
 define ('dbuser',  'colegioa_chromeuser');
 define ('dbpass',  'mateus@2023');
 define ('db',  'colegioacontrolechrome');
