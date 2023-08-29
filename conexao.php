e4812848a5cd384a41c82118a87007ed4047671b
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-08-29 20:11:52 -0300

    azure conexao.php

--- conexao.php
+++ conexao.php
@@ -3,7 +3,20 @@
  $dbuser = "colegioa_chromeuser";
  $dbpass = "mateus@2023";
  $db = "colegioacontrolechrome";
- 
- $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $dbhost, $dbuser, $dbpass, $db, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
+
+
