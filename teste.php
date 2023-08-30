8a1cf5bfe0cf4cd9973da0cb1d42ebfec810691c
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-08-30 12:29:37 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,17 +1,24 @@
 <?php
-$servername = "chromelocalhost.mysql.database.azure.com";
-$username = "colegioa_chromeuser";
-$password = "mateus@2023";
-$database = "colegioacontrolechrome";
+$host = 'chromelocalhost.mysql.database.azure.com';
+$username = 'colegioa_chromeuser';
+$password = 'mateus@2023';
+$db_name = 'colegioacontrolechrome';
 
-// Estabeleça a conexão
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Verifique a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+// Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+// If connection failed, show the error
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-echo "Conexão bem-sucedida!";
-$conn->close();
+echo "Connection successful!";
+mysqli_close($conn);
+
 ?>
