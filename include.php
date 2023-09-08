619d0de85728a5ae32415a95be047c7b6e5b62ec
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-09-08 10:01:49 -0300

    agora vai include.php

--- include.php
+++ include.php
@@ -1,22 +1,28 @@
 
 <?php
- $dbhost = "chromelocalhost.mysql.database.azure.com";
- $dbuser = "colegioa_chromeuser";
- $dbpass = "mateus@2023";
- $db = "colegioacontrolechrome";
+$dbhost = "chromelocalhost.mysql.database.azure.com";
+$dbuser = "colegioa_chromeuser";
+$dbpass = "mateus@2023";
+$db = "colegiocontrolechrome";
 
-//Initializes MySQLi
+// Inicializa o MySQLi
 $conn = mysqli_init();
 
-mysqli_ssl_set($conn,NULL,NULL, "https://chrome.vault.azure.net/certificates/Controlbook/8020b26d86554f648c977c6d1cd978ec", NULL, NULL);
+// Estabelece a conexão
+mysqli_real_connect(
+    $conn,
+    $dbhost,
+    $dbuser,
+    $dbpass,
+    $db,
+    3306,
+    NULL,
+    0 // Sem SSL
+);
 
-// Establish the connection
-mysqli_real_connect($conn, 'chromelocalhost.mysql.database.azure.com', 'colegioa_chromeuser', 'mateus@2023', 'colegioacontrolechrome', 3306, NULL, MYSQLI_CLIENT_SSL);
-
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+// Se a conexão falhar, exibe o erro
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 // Obtém os dados do formulário e previne ataques de injeção de SQL
