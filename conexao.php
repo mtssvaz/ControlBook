3d2298dc1f03f38e7cb6f5d22d3bcfc66f509e3d
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-09-08 09:43:25 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,22 +1,39 @@
 <?php
- $dbhost = "chromelocalhost.mysql.database.azure.com";
- $dbuser = "colegioa_chromeuser";
- $dbpass = "mateus@2023";
- $db = "colegiocontrolechrome";
+$dbhost = "chromelocalhost.mysql.database.azure.com";
+$dbuser = "colegioa_chromeuser";
+$dbpass = "mateus@2023";
+$db = "colegioacontrolechrome";
 
-//Initializes MySQLi
+// Initializes MySQLi
 $conn = mysqli_init();
 
-mysqli_ssl_set($conn,NULL,NULL, "https://chrome.vault.azure.net/certificates/Controlbook/8020b26d86554f648c977c6d1cd978ec", NULL, NULL);
+// Configura a conexão SSL
+mysqli_ssl_set(
+    $conn,
+    NULL, // Deve ser NULL se você não tem seu próprio arquivo de certificado
+    NULL,
+    "/var/www/html/ca-certificate.crt", // Caminho para o arquivo de certificado CA
+    NULL,
+    NULL
+);
 
-// Establish the connection
-mysqli_real_connect($conn, 'chromelocalhost.mysql.database.azure.com', 'colegioa_chromeuser', 'mateus@2023', 'colegioacontrolechrome', 3306, NULL, MYSQLI_CLIENT_SSL);
+// Estabelece a conexão
+mysqli_real_connect(
+    $conn,
+    $dbhost,
+    $dbuser,
+    $dbpass,
+    $db,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+// Se a conexão falhar, exibe o erro
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 ?>
+
 
 
