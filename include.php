bdc2505efc55d1869d91b0c34e599865fc9b5385
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-09-20 01:33:21 -0300

    Update include.php

--- include.php
+++ include.php
@@ -36,15 +36,7 @@ $conservacao = mysqli_real_escape_string($conn, $_POST['conservacao']);
 // Prepara uma consulta SQL segura para inserir os dados na tabela
 $sql = "INSERT INTO cadastro (aluno, responsavel, MATRICULA, n_de_serie, dt_entrega, conservacao) VALUES ('$aluno', '$responsavel', '$MATRICULA', '$n_de_serie', '$dt_entrega', '$conservacao')";
 
-// Obtém os dados do formulário e previne ataques de injeção de SQL
-$serial = mysqli_real_escape_string($conn, $_POST['serial']);
-$modelo = mysqli_real_escape_string($conn, $_POST['modelo']);
-$ID = mysqli_real_escape_string($conn, $_POST['ID']);
-$dt_entrada = mysqli_real_escape_string($conn, $_POST['dt_entrada']);
 
-
-// Prepara uma consulta SQL segura para inserir os dados na tabela
-$sql = "INSERT INTO cadastrochrome (serial, modelo, ID, dt_entrada, ) VALUES ('$serial', '$modelo', '$ID', '$dt_entrada',)";
 
 // Executa a consulta e verifica se foi bem-sucedida
 if (mysqli_query($conn, $sql)) {
