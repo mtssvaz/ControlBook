637b845fd4bed247fce29a9542484dd023a106e3
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-09-28 00:43:49 -0300

    Update aeditarchrome.php

--- aeditarchrome.php
+++ aeditarchrome.php
@@ -16,33 +16,38 @@ mysqli_real_connect(
     $db,
     3306,
     NULL,
-    0 // Sem SSL
+    MYSQLI_CLIENT_SSL // Use MYSQLI_CLIENT_SSL para SSL
 );
-// Obtém os dados do formulário
-$serial = $_POST['serial'];
-$modelo = $_POST['modelo'];
-$ID = $_POST['ID'];
-$dt_entrada = $_POST['dt_entrada'];
-$localizacao = $_POST['localizacao'];
 
-// Prepara uma consulta SQL para atualizar os dados na tabela
+// Verifica a conexão
+if (mysqli_connect_errno()) {
+    die('Falha na conexão com o MySQL: ' . mysqli_connect_error());
+}
+
+// Obtém os dados do formulário e previne ataques de injeção de SQL
+$serial = mysqli_real_escape_string($conn, $_POST['serial']);
+$modelo = mysqli_real_escape_string($conn, $_POST['modelo']);
+$ID = mysqli_real_escape_string($conn, $_POST['ID']);
+$dt_entrada = mysqli_real_escape_string($conn, $_POST['dt_entrada']);
+$localizacao = mysqli_real_escape_string($conn, $_POST['localizacao']);
+
+// Prepara uma consulta SQL segura para atualizar os dados na tabela
 $sql = "UPDATE CADASTRO SET
             serial='{$serial}',
             modelo='{$modelo}',
-            ID='{$ID}',
             dt_entrada='{$dt_entrada}',
             localizacao='{$localizacao}'
         WHERE
-            ID='{$_POST["ID"]}'"; // Substitui REQUEST por POST e adiciona aspas simples
+            ID='{$ID}'";
 
-// Executa a consulta e verifica se foi bem sucedida
+// Executa a consulta e verifica se foi bem-sucedida
 if (mysqli_query($conn, $sql)) {
-  print "<script>alert('Cadastro editado com sucesso!');</script>";
-  print "<script>location.href='estoque.php';</script>";
+    echo "<script>alert('Cadastro editado com sucesso!');</script>";
+    echo "<script>location.href='estoque.php';</script>";
 } else {
-  print "Erro ao cadastrar: " . mysqli_error($conn);
+    echo "Erro ao editar cadastro: " . mysqli_error($conn);
 }
 
-// Fecha a conex���o
+// Fecha a conexão
 mysqli_close($conn);
 ?>
