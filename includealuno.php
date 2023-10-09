1687c8a4e0b6df9920f115b426d247d650299c0f
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-10-09 15:44:44 -0300

    Update includealuno.php

--- includealuno.php
+++ includealuno.php
@@ -28,14 +28,16 @@ if (mysqli_connect_errno()) {
 // Obtém os dados do formulário e previne ataques de injeção de SQL
 $aluno = mysqli_real_escape_string($conn, $_POST['aluno']);
 $responsavel = mysqli_real_escape_string($conn, $_POST['responsavel']);
+$ano = mysqli_real_escape_string($conn, $_POST['ano']);
 $cep = mysqli_real_escape_string($conn, $_POST['cep']);
+$numero = mysqli_real_escape_string($conn, $_POST['numero']);
 $rua = mysqli_real_escape_string($conn, $_POST['rua']);
 $bairro = mysqli_real_escape_string($conn, $_POST['bairro']);
 $cidade = mysqli_real_escape_string($conn, $_POST['cidade']);
 $uf = mysqli_real_escape_string($conn, $_POST['uf']);
 
 // Prepara uma consulta SQL segura para inserir os dados na tabela
-$sql = "INSERT INTO cadastroaluno (aluno, responsavel, cep, rua, bairro, cidade, uf) VALUES ('$aluno', '$responsavel', '$cep', '$rua', '$bairro', '$cidade', '$uf')";
+$sql = "INSERT INTO cadastroaluno (aluno, responsavel, ano, cep, rua, numero, bairro, cidade, uf) VALUES ('$aluno', '$responsavel', '$ano', '$cep', '$numero', '$rua', '$bairro', '$cidade', '$uf')";
 
 
 
