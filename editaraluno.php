7e2687d8775b20d1a4514dcacf67c12954421dc8
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-10-10 02:24:13 -0300

    Update editaraluno.php

--- editaraluno.php
+++ editaraluno.php
@@ -32,19 +32,8 @@ if (!empty($_GET['ID'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 
-    <link rel="shortcut icon" href="imagens/fav_icon.png" type="image/x-icon"/>
-
-    <link rel="stylesheet" href="/styles/cadastrar.css" />
-
-
+    <!-- Restante das suas importações de CSS e JavaScript permanecem inalteradas -->
 
     <title>Editar Cadastro</title>
 </head>
