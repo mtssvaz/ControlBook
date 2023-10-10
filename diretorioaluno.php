e2a4b919553ff25e2d0c2fe00191a26dcb6185a2
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-10-10 03:39:58 -0300

    Update diretorioaluno.php

--- diretorioaluno.php
+++ diretorioaluno.php
@@ -132,7 +132,7 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
 			<table id="tabela" class="table table-hover">
 				<thead>
 					<tr>
-					    <th>ID</th>
+					    <th>RM</th>
 						<th>Aluno</th>
 						<th>Responsavel</th>
 						<th>Ano</th>
